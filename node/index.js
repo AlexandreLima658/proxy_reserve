1944224d853990ccb273ea181a403ef0772c3ebc
alexandrelima658 <alexandrec658@gmail.com>
2024-01-06 15:06:25 -0300

    proxy reserve

---
+++ node/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'node'
+};
+
+app.get('/', (req, res) =>{
+    const connection = mysql.createConnection(config)
+    const sql = 'INSERT INTO people(name) values ("Liana Chagas")'
+    connection.query(sql)
+    connection.query("SELECT * FROM people", (err, result) =>{
+        if (err){
+            throw err
+        }
+        const array = []
+        for (let i=0; i < result.length; i ++){
+            array.push(result[i].name)
+        }
+        const list =  array.join('<li></li>')
+        res.send(`
+            <h1>Full Cycle Rocks!</h1>
+                <ul>
+                    <li>${list}</li>
+                </ul>
+        `)
+        connection.end()
+    })
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
+
---
+++ sql/sql.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS node;
+USE node;
+
+CREATE TABLE IF NOT EXISTS people(
+    id int not null auto_increment,
+    name varchar(255),
+    primary key(id)
+);
